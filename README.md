baaab7059d833aaad1ca1dfc31f5ad2e3b6be976
Chimit Menzhikov <chimit@mosk.ru>
2020-02-24 00:08:23 +0700

    Second step

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# frontend-project-lvl1
+# frontend-project-lvl2
 [![Maintainability](https://api.codeclimate.com/v1/badges/909551a3a8f717d4a16a/maintainability)](https://codeclimate.com/github/menzhikov/frontend-project-lvl2/maintainability)
 [![Build Status](https://travis-ci.org/menzhikov/frontend-project-lvl2.svg?branch=master)](https://travis-ci.org/menzhikov/frontend-project-lvl2)
 
--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,6 +4,12 @@ import program from 'commander';
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    console.log(firstConfig);
+    console.log(secondConfig);
+  });
 
 program.parse(process.argv);
