ee6dbacd4868f97ae7e61dd6fe024d1bb470ee59
Chimit Menzhikov <chimit@mosk.ru>
2020-02-26 22:28:30 +0700

    Refactor code

--- src/index.js
+++ src/index.js
@@ -29,36 +29,30 @@ const genDiff = (firstConfig, secondConfig) => {
   const objectAfter = JSON.parse(
     fs.readFileSync(path.resolve(process.cwd(), secondConfig), encoding),
   );
-
   const uniqKeys = _.uniq([...Object.keys(objectBefore), ...Object.keys(objectAfter)]);
-
   const changes = uniqKeys.map((key) => {
-    if (_.has(objectAfter, key) && _.has(objectBefore, key)
-        && objectAfter[key] === objectBefore[key]) {
-      return { key: [key], value: objectBefore[key], state: states.unchanged };
+    const hasObjectAfterKey = _.has(objectAfter, key);
+    const hasObjectBeforeKey = _.has(objectBefore, key);
+    const isEqualValuesByKey = objectAfter[key] === objectBefore[key];
+    let result;
+    if (hasObjectAfterKey && hasObjectBeforeKey && isEqualValuesByKey) {
+      result = { key: [key], value: objectBefore[key], state: states.unchanged };
     }
-
-    if (_.has(objectAfter, key) && _.has(objectBefore, key)
-        && objectAfter[key] !== objectBefore[key]) {
+    if (hasObjectAfterKey && hasObjectBeforeKey && !isEqualValuesByKey) {
       const oldValue = { key: [key], value: objectBefore[key], state: states.old };
       const newValue = { key: [key], value: objectAfter[key], state: states.new };
-      return [oldValue, newValue];
+      result = [oldValue, newValue];
     }
-
-    if (_.has(objectAfter, key) && !_.has(objectBefore, key)) {
-      return { key: [key], value: objectAfter[key], state: states.new };
+    if (hasObjectAfterKey && !hasObjectBeforeKey) {
+      result = { key: [key], value: objectAfter[key], state: states.new };
     }
-
-    if (!_.has(objectAfter, key) && _.has(objectBefore, key)) {
-      return { key: [key], value: objectBefore[key], state: states.old };
+    if (!hasObjectAfterKey && hasObjectBeforeKey) {
+      result = { key: [key], value: objectBefore[key], state: states.old };
     }
+    return result;
+  });
 
-    return null;
-  }).filter((x) => x !== null);
-
-  const resultString = format(_.flatten(changes));
-
-  return resultString;
+  return format(_.flatten(changes));
 };
 
 export default genDiff;
